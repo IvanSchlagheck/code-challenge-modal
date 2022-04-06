c153b3643d12f5998fd2f1991171706fc95a5cbb
ivan schlagheck <ivansch05@gmail.com>
2022-04-06 18:03:56 +0200

    controller actions implemented

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,26 @@
 class InvitationsController < ApplicationController
+  def new 
+    @invitation = Invitation.new
+    @cycle = Cycle.find(params[:cycle_id])
+
+    respond_to do |el|
+        el.js
+    end
+  end
+
+  def create
+    @invitation = Invitation.new(invitations_params)
+
+    if @invitation.save
+        redirect_to cycles_path , notice: 'All Set! Your invitation was created successfully!'
+    else
+        redirect_to cycles_path 
+    end
+  end
+
+  private
+
+  def invitations_params
+    params.require(:email, :message, :cycle_id, :name)
+  end
 end
