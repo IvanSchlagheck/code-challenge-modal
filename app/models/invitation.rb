60be0a65de97d025806aa45bbb41f41626920be9
ivan schlagheck <ivansch05@gmail.com>
2022-04-06 18:09:52 +0200

    created and edited views and form

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,3 +1,14 @@
 class Invitation < ApplicationRecord
-  belongs_to :cycle
+  belongs_to :cycle # i conect it to the model that it belongs to.
+
+  before_validation :emailHandler #before every vaidation i check if the there is at least an input calling the private fuction.
+
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } # then we validate that an email is actually present and that has the right format.
+  
+
+  private
+
+  def emailHandler
+    return if email.nil? || email === '' # checks if there is at least an input
+  end
 end
